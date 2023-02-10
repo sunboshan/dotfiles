f726ace33ca2891c5f507d69f04f903e97d1ccdc
Boshan Sun <sunbosha@amazon.com>
2023-02-09 22:58:41 -0800

    Add tampermonkey_clean_youtube script

---
+++ tampermonkey_clean_youtube.js
@@ -0,0 +1,37 @@
+// ==UserScript==
+// @name         Clean Youtube
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Turnning off recommendations on youtube
+// @author       You
+// @match        https://www.youtube.com/
+// @include      https://www.youtube.com/watch*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=youtube.com
+// @grant        none
+// ==/UserScript==
+
+(function () {
+  'use strict';
+
+  var css = `
+  #contents.ytd-rich-grid-renderer {
+    display: none;
+  }
+
+  #secondary {
+    display: none;
+  }
+`;
+
+  function addGlobalStyle(css) { // stolen from http://greasemonkey.win-start.de/patterns/add-css.html
+    var head, style;
+    head = document.getElementsByTagName('head')[0];
+    if (!head) { return; }
+    style = document.createElement('style');
+    style.type = 'text/css';
+    style.innerHTML = css;
+    head.appendChild(style);
+  }
+
+  addGlobalStyle(css);
+})();
